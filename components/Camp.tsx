6dff591c1a7439eaa1e52fbf4f9bd1022a6d18dc
SaMMYFrosT221b <ratnakarg@iitbhilai.ac.in>
2024-01-11 22:13:24 +0530

    added Camp section

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,11 +1,97 @@
-import React from 'react';
+import React from "react";
+import Image from "next/image";
+import { PEOPLE_URL } from "@/constants";
 
-const Camp = () =>{
-    return (
-        <div>
-            Camp
-        </div>
-    )
+interface CampProps {
+  backgroundImage: string;
+  title: string;
+  subTitle: string;
+  peopleJoined: string;
 }
+
+const CapmSite = ({
+  backgroundImage,
+  title,
+  subTitle,
+  peopleJoined,
+}: CampProps) => {
+  return (
+    <div
+      className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}
+    >
+      <div className="flex h-full flex-col items-start justify-between p-6 lg:pz-20 lg:py-10 ">
+        <div className="flexCenter gap-4">
+          <div className="rounded-full bg-green-50 p-4">
+            <Image src="/folded-map.svg" alt="map" width={28} height={28} />
+          </div>
+          <div className="flex flex-col gap-1">
+            <h4 className="bold-18 text-white">{title}</h4>
+            <p className="regular-14 text-white">{subTitle}</p>
+          </div>
+        </div>
+
+        <div className="flecCenter gap-6">
+          <span className="flex -space-x-4 overflow-hidden">
+            {PEOPLE_URL.map((url) => (
+              <Image
+                className="inline-block h-10 w-10 rounded-full"
+                src={url}
+                alt="person"
+                key={url}
+                width={52}
+                height={52}
+              />
+            ))}
+          </span>
+          <p className="bold-16 md:bold-20 text-white">{peopleJoined}</p>
+        </div>
+      </div>
+      Campsite
+    </div>
+  );
+};
+
+const Camp = () => {
+  return (
+    <section className="  2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
+      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+        <CapmSite
+          backgroundImage="bg-bg-img-1"
+          title="Putuk Truno Camp"
+          subTitle="Prigen, Pasuruan"
+          peopleJoined="50+ Joined"
+        />
+        <CapmSite
+          backgroundImage="bg-bg-img-1"
+          title="Mountain View"
+          subTitle="Somewere in the wild"
+          peopleJoined="50+ Joined"
+        />
+      </div>
+      <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
+        <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
+
+            <h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-white">
+
+                <strong> Feeling Lost</strong>
+                And not knowing the way?
+            </h2>
+            <p className="regualr-14 xl:regular-16 mt-5 text-white">
+                Starting from the anexity of the climber when visitin a new climbing location, the possibility of getting lsot is very large. That's why we are here for those of you who wan tto start an adventure
+            </p>
+
+            <Image
+            className="camp-quote"
+            src="/quote.svg"
+            alt="camp-2"
+            width={186}
+            height={219}
+            />
+
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Camp;
